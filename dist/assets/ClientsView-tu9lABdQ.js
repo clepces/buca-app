c44676ac9bdee7a9686a78bd53bff78d595f84c5
Clepces <elexdy-publicen@hotmail.com>
2025-10-16 23:28:36 -0400

    Organize: Move files to public static y others

---
+++ dist/assets/ClientsView-tu9lABdQ.js
@@ -0,0 +1,4 @@
+import{E as i}from"./EmptyState-DllPb4lQ.js";import{L as s}from"./index-D79SGgO_.js";function r(e,t){return e.innerHTML=`
+        <h2 class="view-title"><i class="fas fa-users"></i> Clientes</h2>
+        ${i({icon:"fa-user-plus",message:"La gestión de clientes estará disponible próximamente."})}
+    `,()=>{s.info("Limpiando ClientsView...")}}export{r as ClientsView};
---
+++ dist/assets/PosView-C56nScmU.js
@@ -0,0 +1,4 @@
+import{E as a}from"./EmptyState-DllPb4lQ.js";import{L as i}from"./index-D79SGgO_.js";function o(e,t){return e.innerHTML=`
+        <h2 class="view-title"><i class="fas fa-keyboard"></i> Punto de Venta (POS)</h2>
+        ${a({icon:"fa-cash-register",message:"La interfaz principal de ventas se construirá aquí."})}
+    `,()=>{i.info("Limpiando PosView...")}}export{o as PosView};
